8d3c0d884d6067e55dc4478116158ec172334a68
cneely33 <cneely33@gmail.com>
2022-05-01 10:24:36 -0500

    Update index.py

--- index.py
+++ index.py
@@ -8,7 +8,7 @@ from flask_login import logout_user, current_user
 from app import app
 from views import success, login, login_fd, logout
 ### connect to pages
-from views.viz import live_temp, orbit
+from views.viz import live_temp
 
 use_port = 8080
 
@@ -22,15 +22,13 @@ fluid=True
 
 # make a navitem 
 nav_item = dbc.Container(children=[
-    dbc.NavItem(dbc.NavLink("Home", href="http://10.1.5.93:{port}/".format(port=use_port), class_name="text-white", external_link=True)),
+    dbc.NavItem(dbc.NavLink("Home", href="/", class_name="text-white", external_link=True)),
     # dbc.NavItem(dbc.NavLink(id='logout', className='ms-2', children=[]))
          ])   
 # make a dropdown
 dropdown = dbc.DropdownMenu(
     children=[
         dbc.DropdownMenuItem("Live Feed", href='/views/viz/live_temp', external_link=False),
-        dbc.DropdownMenuItem(divider=True),
-        dbc.DropdownMenuItem("Orbit", href="/views/viz/orbit", external_link=False),
         # dbc.DropdownMenuItem(divider=True),
         # dbc.DropdownMenuItem("Sales Overview", href="/views/viz/sales_overview", external_link=False),
         # dbc.DropdownMenuItem(divider=True),
@@ -49,15 +47,16 @@ logo_navbar = dbc.Navbar(
             html.A(
                 # Use row and col to control vertical alignment of logo / brand
                 dbc.Row(children=[   
-                        dbc.Col(html.Img(src='/assets/Lipseys_[L]_RGB_Dark.png', height="55px")),
+                    
+                        dbc.Col(html.Img(src='/assets/dash-logo-stripe.svg', height="55px")),
                         ],
                     align="center",
                     class_name="g-0",
                 ),
-                href="https://www.lipseys.com/",
+                # href="https://www.lipseys.com/",
                 style={"textDecoration": "none"},
             ),
-         dbc.Col(children=[dbc.NavbarBrand("Year Review", class_name="ms-2"),
+         dbc.Col(children=[dbc.NavbarBrand("FermTrack", class_name="ms-2"),
                  # html.Div(id='user-name', className='ms-2', children=[]),
                  # html.Div(id='logout', className='ms-2', children=[])
                  ], 
@@ -94,7 +93,6 @@ app.validation_layout = html.Div([
     login_fd.layout,
     logout.layout,
     live_temp.layout,
-    orbit.layout,
 ])
 
 
@@ -138,14 +136,6 @@ def display_page(pathname):
                 # return 'Access Denied'
         else:
             return login_fd.layout
-    elif pathname == '/views/viz/orbit':
-        if current_user.is_authenticated:
-            # if any(x in dash_roles for x in user_roles):
-                return orbit.layout
-            # else:
-                # return 'Access Denied'
-        else:
-            return login_fd.layout
     else:
         return "404 Page Error! Please choose a link"
 
@@ -182,5 +172,5 @@ def toggle_navbar_collapse(n, is_open):
 
 server = app.server
     
-# if __name__ == '__main__':
-#     app.run_server(debug=True, host='0.0.0.0', port=use_port, dev_tools_hot_reload=False)
+if __name__ == '__main__':
+    app.run_server(debug=True, host='0.0.0.0', port=use_port, dev_tools_hot_reload=False)
